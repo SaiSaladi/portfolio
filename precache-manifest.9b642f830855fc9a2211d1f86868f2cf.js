90ac6b1c86503551046ce7057cfa4ebf4ebffa68
sai <sai.saladi@sharpspring.com>
2020-03-12 14:57:29 -0400

    Updates

---
+++ precache-manifest.9b642f830855fc9a2211d1f86868f2cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a76b4efa7ddf918a2d3fc8c0ab6a59b3",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "73840548c0df1914008d",
+    "url": "/portfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0f4d6c823240e55c61ff",
+    "url": "/portfolio/static/js/2.7245b878.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/portfolio/static/js/2.7245b878.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73840548c0df1914008d",
+    "url": "/portfolio/static/js/main.5f9092c2.chunk.js"
+  },
+  {
+    "revision": "7107900f59be1fd95810",
+    "url": "/portfolio/static/js/runtime-main.af0b07c5.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.359abbe597777d0e0164a981b2fa9c23.js"
+  "/portfolio/precache-manifest.9b642f830855fc9a2211d1f86868f2cf.js"
 );
 
 self.addEventListener('message', (event) => {
